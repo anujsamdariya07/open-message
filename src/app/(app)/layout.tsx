1bc4dd83b5dc8b1d58440c23d3d5f32e20d26739
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-09-11 19:52:50 +0530

    message card + navbar components

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
-import AuthProvider from '@/context/AuthProvider';
+import '../globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import Navbar from '@/components/Navbar';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
@@ -28,15 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='dark'>
-      <AuthProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Navbar />
           {children}
-          <Toaster />
         </body>
-      </AuthProvider>
     </html>
   );
 }
